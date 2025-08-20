cfa4e0a04a6d5bff23cfc19b8ca3cf29f7b97fd3
leejaeguen <worms0421@gmail.com>
2025-08-20 12:23:27 +0900

    feat: #3 aggregate생성(User, Stock)

---
+++ stock/src/main/java/stock/aggregate/Stock.java
@@ -0,0 +1,60 @@
+package stock.aggregate;
+
+public class Stock {
+    private String stockName;
+    private int stockPrice;
+    private int minStockPrice;
+    private int maxStockPrice;
+
+    public Stock(){
+    }
+
+    public Stock(String stockName, int stockPrice, int minStockPrice, int maxStockPrice) {
+        this.stockName = stockName;
+        this.stockPrice = stockPrice;
+        this.minStockPrice = minStockPrice;
+        this.maxStockPrice = maxStockPrice;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public int getStockPrice() {
+        return stockPrice;
+    }
+
+    public void setStockPrice(int stockPrice) {
+        this.stockPrice = stockPrice;
+    }
+
+    public int getMinStockPrice() {
+        return minStockPrice;
+    }
+
+    public void setMinStockPrice(int minStockPrice) {
+        this.minStockPrice = minStockPrice;
+    }
+
+    public int getMaxStockPrice() {
+        return maxStockPrice;
+    }
+
+    public void setMaxStockPrice(int maxStockPrice) {
+        this.maxStockPrice = maxStockPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "stockName='" + stockName + '\'' +
+                ", stockPrice=" + stockPrice +
+                ", minStockPrice=" + minStockPrice +
+                ", maxStockPrice=" + maxStockPrice +
+                '}';
+    }
+}
